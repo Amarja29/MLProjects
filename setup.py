3fdfcf2140337b324814e27873fd6b390930d938
Amarja29 <amarjaindapwar@gmail.com>
2023-03-14 16:32:17 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+# For packing the application
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirments(file_path:str)->List[str]:
+    '''
+    this function returns the list of requirments.
+    '''
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        requirments=[req.replace("\n","")for req in requirments]
+    if HYPEN_E_DOT in requirments:
+        requirments.remove(HYPEN_E_DOT)
+        
+    return requirments        
+
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='Amarja',
+    author_email='amarjaindapwar@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirments('requirment.txt')
+    
+)
