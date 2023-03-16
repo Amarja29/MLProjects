6047042bba000d443fcafd58416a93d2e50a3c37
Amarja29 <amarjaindapwar@gmail.com>
2023-03-16 12:02:20 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+# tracking the exceptions and other logs.
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True) #eneven thought there is a file or folder keep on appending it inside the folder
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s %(levelname)s %(message)s",
+    level=logging.INFO,
+)
+
+if __name__=="__main__":
+    logging.info("Logging has started")
---
+++ src/utils.py
@@ -0,0 +1,1 @@
+#any functionalities written and used in a comman way e.g. save the code in cloud, call mongo-db client, etc.
